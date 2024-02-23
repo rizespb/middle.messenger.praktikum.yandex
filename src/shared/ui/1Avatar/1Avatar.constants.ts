de9cf31bfae0b74c702ef6775dcc603f8865138e
Маклачков Александр <aleksandr.maklachkov@gazprombank.ru>
2024-02-23 10:23:41 +0300

    Renamed  Avatar to 1Avatar

--- src/shared/ui/1Avatar/1Avatar.constants.ts
+++ src/shared/ui/1Avatar/1Avatar.constants.ts
@@ -1,4 +1,4 @@
-import { TAvatarSize } from './Avatar.interfaces';
+import { TAvatarSize } from './1Avatar.interfaces';
 import classes from './Avatar.module.scss';
 
 export const sizesMap: Record<TAvatarSize, string> = {
